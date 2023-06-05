f4535cc7a5331361f8d9cb40c1e73bab6f28e365
mdyaserafe <mdyasenrafe@gmail.com>
2023-06-05 18:26:59 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -10,15 +10,13 @@ const demoObj = {
 };
 
 const App = () => {
-  const [tabs, setTabs] = useState([0]);
   const [activeTab, setActiveTab] = useState(0);
   const [tabsData, setTabsData] = useState([demoObj]);
 
   const addTab = () => {
-    if (tabs.length < 5) {
-      setTabs([...tabs, tabs.length]);
+    if (tabsData.length < 5) {
       setTabsData([...tabsData, demoObj]);
-      setActiveTab(tabs.length);
+      setActiveTab(tabsData.length);
     }
   };
 
@@ -27,12 +25,10 @@ const App = () => {
   };
 
   const removeTab = (index) => {
-    const newTabs = tabs.filter((_, i) => i !== index);
     // delete tabs data
     const newTabsData = tabsData.filter((_, i) => i !== index);
     setTabsData(newTabsData);
     setActiveTab(index - 1);
-    setTabs(newTabs);
   };
 
   const handleChange = (e, name, index) => {
@@ -53,7 +49,7 @@ const App = () => {
   return (
     <div className="m-[16px]">
       <div className="tab-bar flex space-x-2">
-        {tabs.map((tabIndex, index) => (
+        {tabsData.map((tabIndex, index) => (
           <div
             key={index}
             className={`cursor-pointer rounded   ${
@@ -65,7 +61,7 @@ const App = () => {
             <div onClick={() => switchTab(index)} className="px-2 py-2">
               <p>TAB {index + 1}</p>
             </div>
-            {tabs.length > 1 && (
+            {tabsData.length > 1 && (
               <GrFormClose
                 className="ml-2 text-red-600"
                 onClick={() => removeTab(index)}
@@ -74,7 +70,7 @@ const App = () => {
             )}
           </div>
         ))}
-        {tabs.length < 5 && (
+        {tabsData.length < 5 && (
           <button
             onClick={addTab}
             className="add-button bg-blue-500 text-white px-4 py-2 rounded"
